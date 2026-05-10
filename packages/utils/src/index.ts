aaeb0f4831aee1695ba27295c7d2af9940d0f667
Peramanathan Sathyamoorthy <sathyam.peram@gmail.com>
2026-05-10 22:46:11 +0530

    feat(utils): simplify exports to clean names now that converters are feature-complete
    Removed unnecessary alias exports (simple_*, incomplete_*, partial_*) which were placeholders indicating incomplete implementation. Now exporting the primary API: getDereferencedOpenAPIDocument, openAPISchemaToZod, zodToOpenAPISchema.

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,9 +1,5 @@
 export {
   getDereferencedOpenAPIDocument,
-  openAPISchemaToZod as simple_openAPISchemaToZod,
-  openAPISchemaToZod as incomplete_openAPISchemaToZod,
-  openAPISchemaToZod as partial_openAPISchemaToZod,
-  zodToOpenAPISchema as simple_zodToOpenAPISchema,
-  zodToOpenAPISchema as incomplete_zodToOpenAPISchema,
-  zodToOpenAPISchema as partial_zodToOpenAPISchema,
+  openAPISchemaToZod,
+  zodToOpenAPISchema,
 } from './openapi';
