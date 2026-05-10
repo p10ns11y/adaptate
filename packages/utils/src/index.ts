1b896e2ba7950b2ed8e1e69f1a0e27f327f0c469
Peramanathan Sathyamoorthy <sathyam.peram@gmail.com>
2026-05-10 21:52:36 +0530

    feat(utils): update index.ts and load-yaml.ts for feature-complete utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,8 +1,5 @@
 export {
   getDereferencedOpenAPIDocument,
-  // Features such as max, min, minLength, maxLength, pattern are missing
-  // Check: https://github.com/StefanTerdell/json-schema-to-zod
-  // And zod-to-json-schema
   openAPISchemaToZod as simple_openAPISchemaToZod,
   openAPISchemaToZod as incomplete_openAPISchemaToZod,
   openAPISchemaToZod as partial_openAPISchemaToZod,
--- packages/utils/src/load-yaml.ts
+++ packages/utils/src/load-yaml.ts
@@ -1,16 +1,16 @@
 import yaml from 'js-yaml';
 
 export async function getYamlContent(fileURL: string, relativePath: string) {
-  let fs = await import('node:fs');
-  let path = await import('node:path');
-  let { fileURLToPath } = await import('node:url');
-  let { dirname } = path;
-  let fileURLPath = fileURLToPath(fileURL);
-  let callerDirectoryName = dirname(fileURLPath);
-  let yamlFilePath = path.resolve(callerDirectoryName, relativePath);
-  let openapiDocument = yaml.load(
+  const fs = await import('node:fs');
+  const path = await import('node:path');
+  const { fileURLToPath } = await import('node:url');
+  const { dirname } = path;
+  const fileURLPath = fileURLToPath(fileURL);
+  const callerDirectoryName = dirname(fileURLPath);
+  const yamlFilePath = path.resolve(callerDirectoryName, relativePath);
+  const openapiDocument = yaml.load(
     fs.readFileSync(yamlFilePath, 'utf8')
-  ) as string;
+  ) as any;
 
   return openapiDocument;
 }
